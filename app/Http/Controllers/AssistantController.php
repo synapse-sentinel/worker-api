e07d671e9d03183c920994a92dbde841621ec0e2
Jordan <jordan.l.partridge@gmail.com>
2024-04-01 17:13:30 -0700

    Assistant model and api setup

--- app/Http/Controllers/AssistantController.php
+++ app/Http/Controllers/AssistantController.php
@@ -2,65 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreassistantRequest;
-use App\Http\Requests\UpdateassistantRequest;
+use App\Http\Requests\StoreAssistantRequest;
+use App\Http\Requests\UpdateAssistantRequest;
 use App\Models\Assistant;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Response;
 
 class AssistantController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): LengthAwarePaginator
     {
-        //
+        return Assistant::query()->paginate();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreassistantRequest $request)
+    public function store(StoreAssistantRequest $request): Assistant
     {
-        //
+        return Assistant::create($request->validated());
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Assistant $assistant)
+    public function show(Assistant $assistant): Assistant
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Assistant $assistant)
-    {
-        //
+        return $assistant;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateassistantRequest $request, Assistant $assistant)
+    public function update(UpdateAssistantRequest $request, Assistant $assistant): Assistant
     {
-        //
+        $assistant->update($request->validated());
+        return $assistant;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Assistant $assistant)
+    public function destroy(Assistant $assistant): Response
     {
-        //
+        $assistant->delete();
+        return response('Deleted', 204);
     }
 }
--- tests/Feature/AssistantTest.php
+++ tests/Feature/AssistantTest.php
@@ -1,7 +1,59 @@
 <?php
 
+use App\Models\Assistant;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
 test('can be created with factory', function () {
-    $model = \App\Models\Assistant::factory()->create();
+    $model = Assistant::factory()->create();
     $this->assertDatabaseHas('assistants', $model->toArray());
     $this->assertModelExists($model);
 });
+
+test('can list over api', function () {
+    Sanctum::actingAs(
+        User::factory()->create());
+
+    $model    = Assistant::factory()->create();
+    $response = $this->get('/api/assistants');
+    $response->assertStatus(200);
+    $response->assertJsonFragment($model->toArray());
+});
+
+test('can retrieve over api', function () {
+    Sanctum::actingAs(
+        User::factory()->create());
+
+    $model    = Assistant::factory()->create();
+    $response = $this->get('/api/assistants/' . $model->id);
+    $response->assertStatus(200);
+    $response->assertJsonFragment($model->toArray());
+});
+
+test('can be created over api', function () {
+    Sanctum::actingAs(
+        User::factory()->create());
+    $this->post('/api/assistants', ['name' => 'Test Assistant'])
+         ->assertStatus(201)
+         ->assertJsonFragment(['name' => 'Test Assistant']);
+});
+
+test('can by soft deleted over api', function () {
+    Sanctum::actingAs(
+        User::factory()->create());
+    $model = Assistant::factory()->create();
+    $this->delete('/api/assistants/' . $model->id)
+         ->assertStatus(204);
+    $this->assertSoftDeleted($model);
+});
+
+test('can be updated over api', function () {
+    Sanctum::actingAs(
+        User::factory()->create());
+    $model = Assistant::factory()->create();
+    $this->put('/api/assistants/' . $model->id, ['name' => 'Updated Assistant'])
+         ->assertStatus(200)
+         ->assertJsonFragment(['name' => 'Updated Assistant']);
+});
+
+
