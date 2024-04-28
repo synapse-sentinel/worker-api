0f2e94e2831e55f2923b915aa938343d242a1da3
Jordan <jordan.l.partridge@gmail.com>
2024-04-28 14:32:06 -0700

    Message boiler plate

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,9 @@ class MessageFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory()->create(),
+            'thread_id' => Thread::factory()->create(),
+            'content' => $this->faker->text,
         ];
     }
 }
