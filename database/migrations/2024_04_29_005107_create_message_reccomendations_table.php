e957713ee1782043b78a936d3e8646503f7372a7
Jordan <jordan.l.partridge@gmail.com>
2024-04-28 20:59:00 -0700

    messages being assigned

---
+++ database/migrations/2024_04_29_005107_create_message_reccomendations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('message_recommendations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('message_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('assistant_id')->constrained()->cascadeOnDelete();
+            $table->string('reason');
+            $table->integer('points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('message_recommendations');
+    }
+};
