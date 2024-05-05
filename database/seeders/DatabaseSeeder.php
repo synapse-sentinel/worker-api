24a03753d3b50a3430895d6fc408ef86ddc8a9f6
Jordan <jordan.l.partridge@gmail.com>
2024-05-05 16:04:16 -0700

    proper seed setup 🌱

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AiModelSeeder::class,
+            AssistantSeeder::class,
             UserSeeder::class,
+
         ]);
     }
 }
