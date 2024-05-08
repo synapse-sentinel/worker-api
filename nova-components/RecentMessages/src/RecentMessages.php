e931aa9037338ad1e9c4748aa147dcb17bbc197d
Jordan <jordan.l.partridge@gmail.com>
2024-05-07 23:21:53 -0700

    add reply card

---
+++ nova-components/RecentMessages/src/RecentMessages.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PartridgeRocks\RecentMessages;
+
+use Illuminate\Support\Facades\Auth;
+use Laravel\Nova\Card;
+
+class RecentMessages extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = 'full';
+
+    /**
+     * Get the component name for the element.
+     */
+    public function component(): string
+    {
+        return 'recent-messages';
+    }
+
+    // in nova-components/RecentMessages/src/RecentMessages.php
+
+    public function jsonSerialize(): array
+    {
+        return array_merge(parent::jsonSerialize(), [
+            'messages' => \App\Models\Message::whereNot('user_id', Auth::user()->getAuthIdentifier())->with('user') // assuming there's a user relationship
+                ->latest()
+                ->take(5)
+                ->get()
+                ->map(function ($message) {
+                    return [
+                        'id' => $message->id,
+                        'username' => $message->user->name, // adjust based on your user model
+                        'created_at' => $message->created_at->toFormattedDateString(), // or any other format you prefer
+                        'content' => $message->content,
+                        'thread' => $message->thread, // adjust based on your message model
+                    ];
+                }),
+        ]);
+    }
+}
