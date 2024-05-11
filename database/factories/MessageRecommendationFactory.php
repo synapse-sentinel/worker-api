10ea35fb124707345f27b55b9d3acfdccad055e8
Jordan <jordan.l.partridge@gmail.com>
2024-05-10 23:47:02 -0700

    wanna write tests

---
+++ database/factories/MessageRecommendationFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\MessageRecommendation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MessageRecommendationFactory extends Factory
+{
+    protected $model = MessageRecommendation::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
